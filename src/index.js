b86755fc2dc1bb749113ac68f17cdf0cad8f08a5
Eduardo Silva <eduardojrodriguessilva@gmail.com>
2022-08-29 14:34:33 -0300

    create account

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
-import express from 'express';
+const express = require('express');
+const { v4: uuid } = require('uuid');
 
 const app = express();
+
+app.use(express.json());
+
+const costumers = [];
+
+app.post("/account", (request, response) => {
+  const { cpf, name } = request.body;
+
+  const id = uuid();
+
+  costumers.push({
+    id,
+    cpf,
+    name,
+    statement: [],
+  });
+
+  return response.status(201).send();
+});
 
 app.listen(3333);
